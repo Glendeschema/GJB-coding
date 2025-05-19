1a640cc4f026be55042a21a9ba8dc54beb2ed208
Glen Tsime <lehlohonologlen@gmail.com>
2025-05-19 20:55:58 +0200

    production code

--- deploy2.ps1
+++ deploy2.ps1
@@ -1,1 +1,14 @@
 #this is deploy 2 code
+
+
+#This is the code to Deploy the application
+
+new-azvm -Name "MyResourceGroup" -Location "East US" -ImageName "WindowsServer" `
+-Size "Standard_DS1_v2" -Credential (Get-Credential) `
+-ResourceGroupName "MyResourceGroup" `
+-VirtualNetworkName "MyVNet" `
+-SubnetName "MySubnet" `
+-PublicIpAddressName "MyPublicIP" `
+-SecurityGroupName "MyNSG" -OpenPorts 80,443
+
+#This is production code
